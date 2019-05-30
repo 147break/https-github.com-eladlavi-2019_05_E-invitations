87f492e9209253e2ea66b20dec266797a23dce5e
Asaf <asafzil99@gmail.com>
2019-05-30 20:25:46 +0300

    1

--- 2019_05_30/src/com/company/Main.java
+++ 2019_05_30/src/com/company/Main.java
@@ -3,13 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        drawRectangle(4, 3, 12, 7);
     }
 
     static void drawRectangle(int x, int y, int width, int height){
+        for (int i = 0; i < y; i++) {
+            System.out.println();
+        }
         for (int i = 0; i < height; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(" ");
+            }
             for (int j = 0; j < width; j++) {
                 boolean isFrame = i == 0 || i == height-1 || j == 0 || j == width-1;
+                System.out.print(isFrame ? "*" : " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void drawX(int size){
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 0; j < size; j++) {
+                boolean isFrame = i == 0 || i == size-1 || j == 0 || j == size-1;
                 System.out.print(isFrame ? "*" : " ");
             }
             System.out.println();
